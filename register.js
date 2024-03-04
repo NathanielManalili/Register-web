f8e968f687fc33507f7fb1d7b41de486ef23217c
Nathaniel Manalili <nthnlmanalili@gmail.com>
2024-03-04 07:56:27 -0800

    Added register web

---
+++ register.js
@@ -0,0 +1,17 @@
+console.log("Hello",43,true,false, []);
+
+let btnRegister = document.querySelector("#btnRegister");
+let txtusername = document.querySelector("#txtusername");
+let txtpass = document.querySelector("#txtpass");
+let txtCpass = document.querySelector("#txtCpass");
+let age = document.querySelector("age");
+
+btnRegister.onclick = function(){
+    register(txtusername.value, txtpass.value, txtCpass.value, age);
+}
+
+function register (username, password, confirmPassword, age){
+   console.log(username, password, confirmPassword, age);
+    // alert("Register button has been clicked");
+
+}
